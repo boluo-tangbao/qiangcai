67564be1a50979256566c6a753edd78bf26ca582
William <william.s.sh@hotmail.com>
2022-04-12 22:46:03 +0800

    更新 美团和叮咚的代码
    1. 更新美团代码
      1.1 优化抢菜过程中出现提示框无法点击的问题;
    2. 更新叮咚代码
      1.2 更新叮咚抢菜过程中出现 今天 已约满 的问题;

--- dingdong_v0.0.1.py
+++ dingdong_v0.0.1.py
@@ -1,4 +1,5 @@
 import os
+import sys
 import time
 import uiautomator2 as u2
 
@@ -21,7 +22,10 @@ def start():
     # 此处需要设置为你的手机的序列号
     id = "415cd4ea"
     d = conn_phone(id)
-    hierarchy_str = ""
+    confirm_hierarchy_str = ""
+    pay_hierarchy_set = ""
+    index_today = 0
+    index_full = 5015
     recv_time_list = []
 
     while True:
@@ -33,8 +37,8 @@ def start():
             d(textContains="结算").click()
 
         if d(text="确认订单").exists:
-            hierarchy_str = d.dump_hierarchy()
-            if "自动尝试" in hierarchy_str:
+            confirm_hierarchy_str = d.dump_hierarchy()
+            if "自动尝试" in confirm_hierarchy_str:
                 d.press("back")
                 break
             while True:
@@ -44,10 +48,17 @@ def start():
                 if d(text="立即支付").exists:
                     d(text="立即支付").click()
                     if d(text="选择送达时间").exists:
+                        pay_hierarchy_set = d.dump_hierarchy()
+                        index_today = pay_hierarchy_set.find("今天")
+                        if "已约满" in pay_hierarchy_set[index_today:index_full]:
+                            print("抱歉,今天叮咚运力已满,建议您更换其它平台抢菜,疫情期间,还请保护好自己")
+                            sys.exit(0)
                         for elem in d.xpath("//android.widget.TextView").all():
                             if "-" in elem.text:
                                 recv_time_list.append(elem.text)
-                    d(text=recv_time_list[-1]).click()
+                        d(text=recv_time_list[-1]).click()
+
+                    
 if __name__ == '__main__':
     print("叮咚抢菜插件程序正在运行中....")
     start()
--- meituan_v0.01.py
+++ meituan_v0.01.py
@@ -30,18 +30,19 @@ def start():
 
         if d(textContains="结算").exists:
             d(textContains="结算").click()
-
-        if d(text="提交订单").exists:
-            while True:
-                if d(text="我知道了").exists:
-                    d(text="我知道了").click()
-                if d(text="立即支付").exists:
-                    d(text="立即支付").click()
-                    if d(text="时间").exists:
-                        for elem in d.xpath("//android.widget.TextView").all():
-                            if "-" in elem.text:
-                                recv_time_list.append(elem.text)
-                    d(text=recv_time_list[-1]).click()
+        if d(text="返回购物车").exists:
+            d(text="返回购物车").click()
+           
+        if d(text="确定").exists:
+            d(text="确定").click()
+            
+        if d(text="立即支付").exists:
+            d(text="立即支付").click()
+            if d(text="时间").exists:
+                for elem in d.xpath("//android.widget.TextView").all():
+                    if "-" in elem.text:
+                        recv_time_list.append(elem.text)
+            d(text=recv_time_list[-1]).click()
 if __name__ == '__main__':
     print("美团抢菜插件程序正在运行中....")
     start()
